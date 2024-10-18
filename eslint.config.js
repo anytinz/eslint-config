236e71045a828c2b07ee9c13aec928d55b6c35b2
anytinz <anytinz.ovo@gmail.com>
2024-10-18 08:26:04 +0000

    chore: remove unused eslint ingores

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,6 @@ import { anytinz } from '@anytinz/eslint-config'
 
 export default anytinz({
   ingores: [
-    '**/.editorconfig',
-    '**/.gitignore',
-    '**/.npmrc',
     'src/types/rules/*.d.ts',
   ],
 })
